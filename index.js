091c75ec431d1916733aa655e3a31826d6b28cc5
aelghorfy <kcmamin9@gmail.com>
2024-10-30 15:23:46 +0100

    icon clickable+menu

--- index.js
+++ index.js
@@ -15,3 +15,62 @@ async function myJson(url) {
     return data;
 }
 
+function myLogo() {
+    document.getElementById("iconLancement").addEventListener("click", function(e) {
+        document.getElementById("iconLancement").style.display = "none";
+        document.getElementById("menuGestionnaire").style.display = "block"; 
+        AfficherMenu();
+        e.preventDefault();
+    });
+}
+
+myLogo();
+
+function AfficherMenu() {
+    const menuDiv = document.getElementById("menuGestionnaire");
+    menuDiv.innerHTML = "";
+    
+    const titre = document.createElement("h1");
+    titre.textContent = "DynaTask";
+    menuDiv.style.textAlign = "center";
+    menuDiv.appendChild(titre);
+
+    const buttonDetails = [
+        { text: "Ajouter", id: "btn1" },
+        { text: "Afficher", id: "btn2" },
+        { text: "JSP", id: "btn3" }
+    ];
+
+    buttonDetails.forEach(detail => {
+        const button = document.createElement('button');
+        button.textContent = detail.text;
+        button.id = detail.id;
+        button.style.margin = "10px";
+        button.addEventListener("click", () => loadPage(detail.id));
+        menuDiv.appendChild(button);
+    });
+
+    menuDiv.style.display = "block";
+}
+
+function loadPage(pageId) {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "";
+    let content;
+    switch (pageId) {
+        case "btn1":
+            content = "<h2>Ajouter une tâche</h2><p>form d'ajout.</p>";
+            break;
+        case "btn2":
+            content = "<h2>Afficher mes tâches</h2><p>calendrier avec les taches et un select pour les diff options de tri.</p>";
+            break;
+        case "btn3":
+            content = "<h2>Aucune idée</h2><p>à voir.</p>";
+            break;
+        default:
+            content = "<h2>Welcome</h2><p>Select a page to view its content.</p>";
+    }
+
+    contentDiv.innerHTML = content;
+    contentDiv.style.display = "block";
+}
